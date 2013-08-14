59322c502a2f75a8d67b84742a73875a5acda6a8
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-08-14 11:45:12 -0700

    fix spec, part two

--- spec/provider/cloudcontrol_spec.rb
+++ spec/provider/cloudcontrol_spec.rb
@@ -14,7 +14,6 @@ describe DPL::Provider::CloudControl do
   describe 'constructor' do
     it 'with wrong arguments' do
       expect {
-        $stdout.should receive(:write).at_least(3).times
         described_class.new(DummyContext.new, :foo_dep => 'foo_app/default', :email => 'foo@test.com', :password => 'password')
       }.to raise_error(DPL::Error)
     end
