b0f1b351eb1be212ba85a117835f702e9fe95438
Aaron Hill <aa1ronham@gmail.com>
2013-10-13 20:25:26 -0400

    Added Appfog support

--- lib/dpl/provider.rb
+++ lib/dpl/provider.rb
@@ -6,6 +6,7 @@ module DPL
     include FileUtils
 
     autoload :Heroku,       'dpl/provider/heroku'
+    autoload :Appfog,       'dpl/provider/appfog'
     autoload :EngineYard,   'dpl/provider/engine_yard'
     autoload :DotCloud,     'dpl/provider/dot_cloud'
     autoload :Nodejitsu,    'dpl/provider/nodejitsu'
