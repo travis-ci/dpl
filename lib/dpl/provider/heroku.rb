c2846fe5c413201d55d838bd12aa85e0d866976e
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-12-15 02:45:52 +0100

    Revert "switch default heroku strategy to git"
    This reverts commit d4fff2fb66aedb93b9d4dadf0281623de937ce06.

--- lib/dpl/provider/heroku.rb
+++ lib/dpl/provider/heroku.rb
@@ -7,7 +7,7 @@ module DPL
       extend self
 
       def new(context, options)
-        strategy = options[:strategy] || 'git'
+        strategy = options[:strategy] || 'anvil'
         constant = constants.detect { |c| c.to_s.downcase == strategy }
         raise Error, 'unknown strategy %p' % strategy unless constant
         const_get(constant).new(context, options)
