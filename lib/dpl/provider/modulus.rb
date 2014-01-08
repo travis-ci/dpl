f54bedd702ecd2aa324ac0f04ce42c877a524eb4
Matt Hernandez <matt@modulus.io>
2014-01-08 16:06:47 -0500

    Added rspec test.
    Updated the validate the api key and project name parameters as well.

--- lib/dpl/provider/modulus.rb
+++ lib/dpl/provider/modulus.rb
@@ -4,9 +4,11 @@ module DPL
       npm_g 'modulus'
 
       def check_auth
+        raise Error, "must supply an api key" unless option(:api_key)
       end
 
       def check_app
+        raise Error, "must supply a project name" unless option(:project_name)
       end
 
       def needs_key?
