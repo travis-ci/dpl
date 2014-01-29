09c9066c820180c65ef5c77c03aad950a5f1ac74
Mathias Meyer <meyer@paperplanes.de>
2014-01-29 14:22:03 +0100

    Set up the git-ssh command for gitdeploykey strategy.
    An unpleasant hack right now, to try out whether this kind of deploy
    works at all.

--- lib/dpl/provider/heroku/git_deploy_key.rb
+++ lib/dpl/provider/heroku/git_deploy_key.rb
@@ -5,6 +5,23 @@ module DPL
         def needs_key?
           false
         end
+
+        def check_auth
+          super
+          setup_git_ssh
+        end
+
+        def setup_git_ssh
+          path = File.expand_path(".dpl/git-ssh")
+
+          File.open(path, 'w') do |file|
+            file.write "#!/bin/sh\n"
+            file.write "exec ssh -o StrictHostKeychecking=no -o CheckHostIP=no -o UserKnownHostsFile=/dev/null -- \"$@\"\n"
+          end
+
+          chmod(0740, path)
+          ENV['GIT_SSH'] = path
+        end
       end
     end
   end
