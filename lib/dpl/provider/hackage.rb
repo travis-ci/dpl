0ee67c42f05dc57c51eed891dc24f256c8b401fa
Alexey Kotlyarov <a@koterpillar.com>
2014-03-27 07:08:09 +1100

    Check auth and cabal check

--- lib/dpl/provider/hackage.rb
+++ lib/dpl/provider/hackage.rb
@@ -3,10 +3,13 @@ module DPL
     class Hackage < Provider
 
       def check_auth
+        unless option(:username) and option(:password)
+          raise Error, "must supply username and password"
+        end
       end
 
       def check_app
-        context.shell "cabal check"
+        context.shell "cabal check" or raise Error, "cabal check failed"
       end
 
       def needs_key?
@@ -14,7 +17,7 @@ module DPL
       end
 
       def push_app
-        context.shell "cabal sdist"
+        context.shell "cabal sdist" or raise Error, "cabal sdist failed"
         Dir.glob("dist/*.tar.gz") do |tar|
           context.shell "cabal upload --username=#{option(:username)} --password=#{option(:password)} #{tar}"
         end
