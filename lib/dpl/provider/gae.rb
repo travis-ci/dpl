227ff9fd074788ddaab6c279955e898925e4bc5b
Hiro Asari <asari.ruby@gmail.com>
2014-09-01 08:47:29 -0400

    Update GAE to 1.9.10

--- lib/dpl/provider/gae.rb
+++ lib/dpl/provider/gae.rb
@@ -7,9 +7,9 @@ module DPL
       experimental 'Google App Engine'
 
       # https://developers.google.com/appengine/downloads
-      GAE_VERSION='1.9.8'
+      GAE_VERSION='1.9.10'
       GAE_ZIP_FILE="google_appengine_#{GAE_VERSION}.zip"
-      SHA1SUM='9835547cd2566ab1eaffe15e4697724eba08831f'
+      SHA1SUM='790fee21ee697434586d3cd36fd2e59f6899a2c3'
       BASE_DIR=Dir.pwd
       GAE_DIR=File.join(BASE_DIR, 'google_appengine')
       APPCFG_BIN=File.join(GAE_DIR, 'appcfg.py')
