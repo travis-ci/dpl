1f2b8c9098db1bd3c0b58d20227733fde073aa42
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-09-23 16:41:25 +0200

    have SDS provider inherit from S3

--- lib/dpl/provider/sds.rb
+++ lib/dpl/provider/sds.rb
@@ -2,14 +2,9 @@ require 'json'
 
 module DPL
   class Provider
-    class SDS < Provider
-      requires 'aws-sdk'
-
-      def needs_key?
-        false
-      end
-
+    class SDS < S3
       def push_app
+        super
       end
     end
   end
