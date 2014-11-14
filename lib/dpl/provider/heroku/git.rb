49db717c3596973518adff2f7a000f2f61e75445
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2014-11-14 21:49:34 +0100

    heroku http git: write token to netrc, so it wont get printed to the logs accidentially

--- lib/dpl/provider/heroku/git.rb
+++ lib/dpl/provider/heroku/git.rb
@@ -2,13 +2,22 @@ module DPL
   class Provider
     module Heroku
       class Git < Generic
+        require 'netrc'
+
         def git_url
-          "https://#{option(:api_key)}@git.heroku.com/#{option(:app)}.git"
+          "https://git.heroku.com/#{option(:app)}.git"
         end
 
         def push_app
+          write_netrc
           git_remote = options[:git] || git_url
           context.shell "git push #{git_remote} HEAD:refs/heads/master -f"
+        end
+
+        def write_netrc
+          n = Netrc.read
+          n['git.heroku.com'] = [user, option(:api_key)]
+          n.save
         end
       end
     end
