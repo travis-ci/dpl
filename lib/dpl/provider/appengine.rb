a3946d7bd507879b42a141855da83d655b30a5b8
Jason Hall <imjasonh@gmail.com>
2015-02-05 14:35:47 -0500

    silently install gcloud

--- lib/dpl/provider/appengine.rb
+++ lib/dpl/provider/appengine.rb
@@ -1,9 +1,37 @@
+require 'open-uri'
+
 module DPL
   class Provider
     class AppEngine < Provider
+      experimental "Google App Engine"
+
+      BASE_DIR=Dir.pwd
+      GCLOUD_ZIP_URL="https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.zip"
+      GCLOUD_ZIP_FILE="google-cloud-sdk.zip"
+
       def self.install_sdk
-        $stderr.puts "Installing Cloud SDK"
-	context.shell "curl https://sdk.cloud.google.com | bash"
+        Dir.chdir(BASE_DIR) do
+            unless File.exists? GCLOUD_ZIP_FILE
+            $stderr.puts "Downloading Google Cloud SDK"
+            File.open(GCLOUD_ZIP_FILE, "wb") do |dest|
+              open(GCLOUD_ZIP_URL, "rb") do |src|
+                dest.write(src.read)
+              end
+            end
+          end
+
+          unless File.directory? "google-cloud-sdk"
+            $stderr.puts "Extracting Google Cloud SDK"
+            Zip::File.open(GCLOUD_ZIP_FILE) do |file|
+              file.each do |entry|
+                entry.extract entry.name
+              end
+            end
+          end
+
+          $stderr.puts "Installing Google Cloud SDK"
+          context.shell "google-cloud-sdk/install.sh --disable-installation-options"
+        end
       end
 
       install_sdk
--- spec/provider/appengine_spec.rb
+++ spec/provider/appengine_spec.rb
@@ -3,10 +3,12 @@ require 'dpl/provider/appengine'
 
 describe DPL::Provider::AppEngine do
   subject :provider do
-    described_class.new(DummyContext.new, :email => 'blah@foo.com', :password => 'bar')
+    described_class.new(DummyContext.new, :user => "foo", :password => "bar")
   end
 
-  describe "#check_auth" do
+  let(:oauth_token) { "TOKEN" }
+
+  describe "#setup_auth" do
     example do
       expect(provider.context).to receive(:shell).with("gcloud auth activate-refresh-token TOKEN")
       provider.setup_auth
