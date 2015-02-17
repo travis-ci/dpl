7693fed5e242b3969e3ecf276a54b0cd683baed8
Gil Tselenchuk <giltsl@gmail.com>
2015-02-17 11:47:47 +0200

    hide sensitive variables from the printable logs

--- lib/dpl/provider/testfairy.rb
+++ lib/dpl/provider/testfairy.rb
@@ -3,10 +3,8 @@ module DPL
                 class TestFairy < Provider
 
                         require "net/http"
-                        # require "uri"
                         require 'net/http/post/multipart'
                         require 'json'
-                        # require 'open-uri'
                         require 'tempfile'
 
 
@@ -18,15 +16,17 @@ module DPL
                         @@UPLOAD_SIGNED_URL_PATH = "/api/upload-signed";
 
                         @@zipPath = "/usr/bin/zip"
-                        @@jarsignerPath = nil #"/usr/bin/jarsigner"
-                        @@zipAlignPath = nil #"/Users/gilt/apps/testfairy_git/server/deployment/bin/darwin/platform-tools/zipalign"
+                        @@jarsignerPath = nil
+                        @@zipAlignPath = nil
 
                         def check_auth
                                 if android?
                                         set_environment
-                                        puts "keystore-file = #{option(:keystore_file)} storepass = #{option(:storepass)} alias = #{option(:alias)}"
+                                        storepassToPrint = option(:storepass).clone
+                                        aliasToPrint = option(:alias).clone
+                                        puts "keystore-file = #{option(:keystore_file)} storepass = #{storepassToPrint.sub! storepassToPrint[1..-2], '****'} alias = #{aliasToPrint.sub! aliasToPrint[1..-2], '****'}"
                                 end
-                                puts "api-key = #{option(:api_key)} symbols-file = #{options[:symbols_file]}"
+                                puts "api-key = #{option(:api_key).gsub(/[123456789]/, '*')} symbols-file = #{options[:symbols_file]}"
                         end
 
                         def android?
@@ -110,17 +110,18 @@ module DPL
                                 uploadSignedUrl = @@SERVER + @@UPLOAD_SIGNED_URL_PATH
 
                                 params = {"api_key" => "#{option(:api_key)}"}
-                                params = add_file_param params , 'apk_file', apkPath
-                                params = add_file_param params, 'symbols_file', options[:symbols_file]
-                                params = add_param params, 'testers-groups', options[:testers_groups]
-                                params = add_boolean_param params, 'notify', options[:notify]
-                                params = add_boolean_param params, 'auto-update', options[:auto_update]
+                                add_file_param params , 'apk_file', apkPath
+                                add_file_param params, 'symbols_file', options[:symbols_file]
+                                add_param params, 'testers-groups', options[:testers_groups]
+                                add_boolean_param params, 'notify', options[:notify]
+                                add_boolean_param params, 'auto-update', options[:auto_update]
 
                                 post uploadSignedUrl, params
                         end
 
                         def post url, params
-                                puts "Upload params = #{JSON.pretty_generate(params)} \n to #{url}"
+
+                                puts "Upload parameters = #{get_printable_params params} \nto #{url}"
                                 uri = URI.parse(url)
                                 request = Net::HTTP::Post::Multipart.new(uri.path, params, 'User-Agent' => "Travis plugin version=#{@@VERSION}")
                                 res = Net::HTTP.start(uri.host, uri.port) do |http|
@@ -134,51 +135,54 @@ module DPL
                                 return resBody
                         end
 
+                        def get_printable_params params
+                                paramsToPrint = params.clone
+                                paramsToPrint['api_key'] = paramsToPrint['api_key'].gsub(/[123456789]/, '*')
+                                paramsToPrint['apk_file'] = paramsToPrint['apk_file'].path()
+                                JSON.pretty_generate(paramsToPrint)
+
+                        end
+
                         def get_params
                                 params = {'api_key' => "#{option(:api_key)}"}
-                                params = add_file_param params, 'apk_file', option(:app_file)
-                                params = add_file_param params, 'symbols_file', options[:symbols_file]
-                                params = add_param params, 'video-quality', options[:video_quality]
-                                params = add_param params, 'screenshot-interval', options[:screenshot_interval]
-                                params = add_param params, 'max-duration', options[:max_duration]
-                                params = add_param params, 'testers-groups', options[:testers_groups]
-                                params = add_param params, 'advanced-options', options[:advanced_options]
-                                params = add_param params, 'metrics', options[:metrics]
-                                params = add_boolean_param params, 'data-only-wifi', options[:data_only_wifi]
-                                params = add_boolean_param params, 'record-on-background', options[:record_on_background]
-                                params = add_boolean_param params, 'video', options[:video]
-                                params = add_boolean_param params, 'notify', options[:notify]
-                                params = add_boolean_param params, 'icon-watermark', options[:icon_watermark]
+                                add_file_param params, 'apk_file', option(:app_file)
+                                add_file_param params, 'symbols_file', options[:symbols_file]
+                                add_param params, 'video-quality', options[:video_quality]
+                                add_param params, 'screenshot-interval', options[:screenshot_interval]
+                                add_param params, 'max-duration', options[:max_duration]
+                                add_param params, 'testers-groups', options[:testers_groups]
+                                add_param params, 'advanced-options', options[:advanced_options]
+                                add_param params, 'metrics', options[:metrics]
+                                add_boolean_param params, 'data-only-wifi', options[:data_only_wifi]
+                                add_boolean_param params, 'record-on-background', options[:record_on_background]
+                                add_boolean_param params, 'video', options[:video]
+                                add_boolean_param params, 'notify', options[:notify]
+                                add_boolean_param params, 'icon-watermark', options[:icon_watermark]
 
                                 travisCommitRange = context.env.fetch('TRAVIS_COMMIT_RANGE',nil)
                                 if !travisCommitRange.nil?
                                         changelog = %x[git log  --pretty=oneline --abbrev-commit #{travisCommitRange}]
                                         params = add_param params, 'changelog', changelog
                                 end
-                                return params
+                                params
                         end
 
                         def add_file_param params, fileName, filePath
                                 if (!filePath.nil? && !filePath.empty?)
-                                        puts "file name last = #{filePath.split("/").last}"
                                         params[fileName] = UploadIO.new(File.new(filePath), "", filePath.split("/").last)
-                                        puts "file name = #{params[fileName]}"
                                 end
-                                return params
                         end
 
                         def add_param params, paramName, param
                                 if (!param.nil? && !param.empty?)
                                         params[paramName] = param
                                 end
-                                return params
                         end
 
                         def add_boolean_param params, paramName, param
                                 if (!param.nil? && !param.empty?)
                                         params[paramName] = (param == true) ? "on" : "off"
                                 end
-                                return params
                         end
                 end
         end
