3cc128c1706e62a282d5ba776d086f609f325317
Hiro Asari <asari.ruby@gmail.com>
2015-07-16 17:44:55 -0400

    Add message about NPM API key change

--- lib/dpl/provider/npm.rb
+++ lib/dpl/provider/npm.rb
@@ -22,6 +22,8 @@ module DPL
       end
 
       def push_app
+        log "NPM API key format changed recently. If your deployment fails, check your API key in ~/.npmrc."
+        log "http://docs.travis-ci.com/user/deployment/npm/"
         context.shell "env NPM_API_KEY=#{option(:api_key)} npm publish"
       end
     end
