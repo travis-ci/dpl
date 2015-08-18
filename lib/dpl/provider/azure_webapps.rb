53b663d207c43b117ba3b2218fa798f16db76000
Felix Rieseberg <felix@felixrieseberg.com>
2015-08-18 12:27:51 -0700

    Add Provider for Azure Web Apps
    - Uses Git to deploy
    - Mandatory parameters: `--username`, `--password`, `--site` (also available as environment variables AZURE_WA_SITE, AZURE_WA_USERNAME, AZURE_WA_PASSWORD)
    - Optional parameter: `--quiet` (don’t print Azure’s deployment output)
    
    Usage: `dpl --provider=AzureWebApps --username=depluser
    --password=deplp@ss --site=dplsite`

---
+++ lib/dpl/provider/azure_webapps.rb
@@ -0,0 +1,40 @@
+module DPL
+  class Provider
+    class AzureWebApps < Provider
+      def config
+        {
+          "username"     => options[:username] || context.env['AZURE_WA_USERNAME'],
+          "password"     => options[:password] || context.env['AZURE_WA_PASSWORD'],
+          "site"         => options[:site] || context.env['AZURE_WA_SITE']
+        }
+      end
+
+      def git_target
+        "https://#{config['username']}:#{config['password']}@#{config['site']}.scm.azurewebsites.net:443/#{config['site']}.git"
+      end
+
+      def needs_key?
+        false
+      end
+
+      def check_app
+      end
+
+      def check_auth
+        error "missing Azure Git Deployment username" unless config['username']
+        error "missing Azure Git Deployment password" unless config['password']
+        error "missing Azure Web App name" unless config['site']
+      end
+
+      def push_app
+        log "Deploying to Azure Web App '#{config['site']}'"
+
+        if !!options[:quiet]
+          context.shell "git push --force --quiet #{git_target} master > /dev/null 2>&1"
+        else
+          context.shell "git push --force --quiet #{git_target} master"
+        end
+      end
+    end
+  end
+end
