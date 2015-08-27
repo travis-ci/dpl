757bf0cbef61b3462ae9ab30c1d74ea0e0781fee
Tim Ysewyn <Tim.Ysewyn@me.com>
2015-08-27 20:31:31 +0200

    Change the way we download, install en run the CF CLI so we can run this on the container-based infrastructure

--- lib/dpl/provider/cloud_foundry.rb
+++ lib/dpl/provider/cloud_foundry.rb
@@ -3,14 +3,13 @@ module DPL
     class CloudFoundry < Provider
 
       def initial_go_tools_install
-        context.shell 'wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb'
-        context.shell 'rm temp.deb'
+        context.shell 'wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-linux-amd64.tgz -qO cf-linux-amd64.tgz && tar -zxvf cf-linux-amd64.tgz && rm cf-linux-amd64.tgz'
       end
 
       def check_auth
         initial_go_tools_install
-        context.shell "cf api #{option(:api)} #{'--skip-ssl-validation' if options[:skip_ssl_validation]}"
-        context.shell "cf login --u #{option(:username)} --p #{option(:password)} --o #{option(:organization)} --s #{option(:space)}"
+        context.shell "./cf api #{option(:api)} #{'--skip-ssl-validation' if options[:skip_ssl_validation]}"
+        context.shell "./cf login --u #{option(:username)} --p #{option(:password)} --o #{option(:organization)} --s #{option(:space)}"
       end
 
       def check_app
@@ -24,8 +23,8 @@ module DPL
       end
 
       def push_app
-        context.shell "cf push#{manifest}"
-        context.shell "cf logout"
+        context.shell "./cf push#{manifest}"
+        context.shell "./cf logout"
       end
 
       def cleanup
