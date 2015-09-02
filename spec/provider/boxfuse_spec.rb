5eecf7a7e2a2bd580bae222383882fd604a224f5
Axel Fontaine <business@axelfontaine.com>
2015-09-02 13:43:24 +0200

    Added spec for Boxfuse

---
+++ spec/provider/boxfuse_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'dpl/provider/boxfuse'
+
+describe DPL::Provider::Boxfuse do
+  subject :provider do
+    described_class.new(DummyContext.new, :user => 'dummyuser')
+  end
+
+  describe "#deploy" do
+    example do
+      expect(provider.context).to receive(:shell).with('curl -L https://files.boxfuse.com/com/boxfuse/client/boxfuse-commandline/latest/boxfuse-commandline-latest-linux-x64.tar.gz | tar xz')
+      expect(provider.context).to receive(:shell).with('boxfuse/boxfuse run -user=dummyuser')
+      provider.deploy
+    end
+  end
+end
