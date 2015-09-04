df8fb89e9c124cb98d4d9319892045af4431532f
Hiro Asari <asari.ruby@gmail.com>
2015-09-04 16:00:39 -0400

    Fix anynines spec, which depends on cloundfoundry spec

--- spec/provider/anynines_spec.rb
+++ spec/provider/anynines_spec.rb
@@ -11,8 +11,7 @@ describe DPL::Provider::Anynines do
 
   describe "#check_auth" do
     example do
-      expect(provider.context).to receive(:shell).with('wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb')
-      expect(provider.context).to receive(:shell).with('rm temp.deb')
+      expect(provider.context).to receive(:shell).with('wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-linux-amd64.tgz -qO cf-linux-amd64.tgz && tar -zxvf cf-linux-amd64.tgz && rm cf-linux-amd64.tgz')
       expect(provider.context).to receive(:shell).with('cf api https://api.de.a9s.eu')
       expect(provider.context).to receive(:shell).with('cf login --u mallomar --p myreallyawesomepassword --o myorg --s outer')
       provider.check_auth
