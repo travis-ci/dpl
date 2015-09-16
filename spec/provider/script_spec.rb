48b9afd29fc167b5efff49793edb73eccfda131f
Hiro Asari <asari.ruby@gmail.com>
2015-09-16 17:57:25 -0400

    Use Open3 to run script provider
    Capture and print STDOUT and STDIN

--- spec/provider/script_spec.rb
+++ spec/provider/script_spec.rb
@@ -8,15 +8,27 @@ describe DPL::Provider::Script do
 
   let(:script) { 'scripts/deploy_script' }
 
+  before :each do
+    stdin, stdout, stderr, wait_thr = double('stdin'), double('stdout'), double('stderr'), double('wait_thr')
+    @status = double
+
+    allow(stdout).to receive(:read)
+    allow(stderr).to receive(:read)
+    allow(wait_thr).to receive(:status).and_return(false)
+    allow(wait_thr).to receive(:value).and_return(@status)
+    allow(@status).to receive(:success?).and_return(true)
+
+    expect(Open3).to receive(:popen3).and_return([stdin, stdout, stderr, wait_thr])
+  end
+
   it 'runs command "script" given' do
-    expect(provider.context).to receive(:shell).with(script)
     provider.push_app
   end
 
   context 'when script exits with nonzero status' do
     before :each do
       # TODO: Found a better way to test this
-      Process::Status.any_instance.stub(:exitstatus).and_return(1)
+      allow(@status).to receive(:success?).and_return(false)
     end
 
     it 'raises error' do
