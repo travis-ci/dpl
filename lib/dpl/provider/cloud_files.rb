7e2777f7b37bbdd3d01bdfbada5a95b6acafa482
Hiro Asari <asari.ruby@gmail.com>
2015-10-21 14:36:40 -0400

    Ping fog to < 1.35.0 for Rackspace
    Fixes https://github.com/travis-ci/travis-ci/issues/4853

--- lib/dpl/provider/cloud_files.rb
+++ lib/dpl/provider/cloud_files.rb
@@ -5,7 +5,7 @@ module DPL
     class CloudFiles < Provider
       requires 'net-ssh',    load: 'net/ssh',    version: '~> 2.9.2' # Anything higher requires Ruby 2.x
       requires 'fog-google', load: 'fog/google', version: '< 0.1.1'  # Anything higher requires Ruby 2.x
-      requires 'fog'
+      requires 'fog', version: '< 1.35.0' # Anything higher requires fog-google 0.1.1 and up, which, in turn, requires Ruby 2.x
       experimental 'Rackspace Cloud Files'
 
       def needs_key?
