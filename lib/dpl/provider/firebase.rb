32e963b7bf2940cd5c74988ad630c73b5d3ae29a
Michael Bleigh <mbleigh@mbleigh.com>
2015-11-15 02:33:25 -0800

    Adds Firebase provider support

---
+++ lib/dpl/provider/firebase.rb
@@ -0,0 +1,26 @@
+module DPL
+  class Provider
+    class Firebase < Provider
+      npm_g 'firebase-tools@^2.1', 'firebase'
+
+      def check_auth
+        raise Error, "must supply token option or FIREBASE_TOKEN environment variable" if !options[:token] && !context.env['FIREBASE_TOKEN']
+      end
+
+      def check_app
+        error "missing firebase.json" unless File.exist? "firebase.json"
+      end
+
+      def needs_key?
+        false
+      end
+
+      def push_app
+        command = "firebase deploy --non-interactive"
+        command << " -f #{options[:project]}" if options[:project]
+        command << " --token '#{options[:token]}'" if options[:token]
+        context.shell command
+      end
+    end
+  end
+end
