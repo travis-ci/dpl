1d6276b4d11b686b68f4180b3b992345a7ef7171
Benjamin Guttmann <bguttmann@avarteq.de>
2015-12-16 13:52:55 +0100

    Fixed provider anynines to work in TravisCI

--- lib/dpl/provider/anynines.rb
+++ lib/dpl/provider/anynines.rb
@@ -4,8 +4,8 @@ module DPL
 
       def check_auth
         initial_go_tools_install
-        context.shell "cf api https://api.de.a9s.eu"
-        context.shell "cf login --u #{option(:username)} --p #{option(:password)} --o #{option(:organization)} --s #{option(:space)}"
+        context.shell "./cf api https://api.de.a9s.eu"
+        context.shell "./cf login --u #{option(:username)} --p #{option(:password)} --o #{option(:organization)} --s #{option(:space)}"
       end
 
     end
