9cc1a755709660e8e0577fe48f1be3c8207a7265
Kepler Sticka-Jones <kepler+git@stickajones.org>
2016-02-05 13:28:14 -0700

    Do Atom installation myself.

--- lib/dpl/provider/apm.rb
+++ lib/dpl/provider/apm.rb
@@ -2,7 +2,6 @@ module DPL
   class Provider
     class APM < Provider
       experimental "Atom Package Manager"
-      context.shell "curl -s https://raw.githubusercontent.com/atom/ci/master/build-package.sh | sh"
       apt_get "build-essential git libgnome-keyring-dev fakeroot"
 
       def needs_key?
@@ -27,6 +26,56 @@ module DPL
       end
 
       def check_auth
+      end
+
+      def check_app
+        context.shell <<-bash
+          echo "Downloading latest Atom release..."
+          ATOM_CHANNEL="${ATOM_CHANNEL:=stable}"
+
+          if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+            curl -s -L "https://atom.io/download/mac?channel=$ATOM_CHANNEL" \
+              -H 'Accept: application/octet-stream' \
+              -o "atom.zip"
+            mkdir atom
+            unzip -q atom.zip -d atom
+            if [ "$ATOM_CHANNEL" = "stable" ]; then
+              export ATOM_APP_NAME="Atom.app"
+              export ATOM_SCRIPT_NAME="atom.sh"
+              export ATOM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh"
+            else
+              export ATOM_APP_NAME="Atom ${ATOM_CHANNEL}.app"
+              export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
+              export ATOM_SCRIPT_PATH="./atom-${ATOM_CHANNEL}"
+              ln -s "./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh" "${ATOM_SCRIPT_PATH}"
+            fi
+            export PATH="$PWD/atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/bin:$PATH"
+            export ATOM_PATH="./atom"
+            export APM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/apm"
+          else
+            curl -s -L "https://atom.io/download/deb?channel=$ATOM_CHANNEL" \
+              -H 'Accept: application/octet-stream' \
+              -o "atom.deb"
+            /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+            export DISPLAY=":99"
+            dpkg-deb -x atom.deb "$HOME/atom"
+            if [ "$ATOM_CHANNEL" = "stable" ]; then
+              export ATOM_SCRIPT_NAME="atom"
+              export APM_SCRIPT_NAME="apm"
+            else
+              export ATOM_SCRIPT_NAME="atom-$ATOM_CHANNEL"
+              export APM_SCRIPT_NAME="apm-$ATOM_CHANNEL"
+            fi
+            export ATOM_SCRIPT_PATH="$HOME/atom/usr/bin/$ATOM_SCRIPT_NAME"
+            export APM_SCRIPT_PATH="$HOME/atom/usr/bin/$APM_SCRIPT_NAME"
+          fi
+
+
+          echo "Using Atom version:"
+          "$ATOM_SCRIPT_PATH" -v
+          echo "Using APM version:"
+          "$APM_SCRIPT_PATH" -v
+        bash
       end
 
       def push_app
