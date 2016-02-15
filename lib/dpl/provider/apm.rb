7ee83e34bd978efc82d2e5f7a00db5aa7d1b0c09
Kepler Sticka-Jones <kepler+git@stickajones.org>
2016-02-15 15:09:49 -0700

    Seperate code and create tests.

--- lib/dpl/provider/apm.rb
+++ lib/dpl/provider/apm.rb
@@ -25,61 +25,78 @@ module DPL
         end
       end
 
-      def check_auth
+      def setup_auth
         context.env['ATOM_ACCESS_TOKEN'] = option(:api_key)
       end
 
-      def push_app
+      def check_auth
+        setup_auth
+      end
+
+      def install_atom
+        log 'Downloading latest Atom release...'
         context.shell <<-bash
-          echo "Downloading latest Atom release..."
-          ATOM_CHANNEL="${ATOM_CHANNEL:=stable}"
+        ATOM_CHANNEL="${ATOM_CHANNEL:=stable}"
 
-          if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-            curl -s -L "https://atom.io/download/mac?channel=$ATOM_CHANNEL" \
-              -H 'Accept: application/octet-stream' \
-              -o "atom.zip"
-            mkdir atom
-            unzip -q atom.zip -d atom
-            if [ "$ATOM_CHANNEL" = "stable" ]; then
-              export ATOM_APP_NAME="Atom.app"
-              export ATOM_SCRIPT_NAME="atom.sh"
-              export ATOM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh"
-            else
-              export ATOM_APP_NAME="Atom ${ATOM_CHANNEL}.app"
-              export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
-              export ATOM_SCRIPT_PATH="./atom-${ATOM_CHANNEL}"
-              ln -s "./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh" "${ATOM_SCRIPT_PATH}"
-            fi
-            export PATH="$PWD/atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/bin:$PATH"
-            export ATOM_PATH="./atom"
-            export APM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/apm"
+        if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+          curl -s -L "https://atom.io/download/mac?channel=$ATOM_CHANNEL" \
+            -H 'Accept: application/octet-stream' \
+            -o "atom.zip"
+          mkdir atom
+          unzip -q atom.zip -d atom
+          if [ "$ATOM_CHANNEL" = "stable" ]; then
+            export ATOM_APP_NAME="Atom.app"
+            export ATOM_SCRIPT_NAME="atom.sh"
+            export ATOM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh"
           else
-            curl -s -L "https://atom.io/download/deb?channel=$ATOM_CHANNEL" \
-              -H 'Accept: application/octet-stream' \
-              -o "atom.deb"
-            /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-            export DISPLAY=":99"
-            dpkg-deb -x atom.deb "$HOME/atom"
-            if [ "$ATOM_CHANNEL" = "stable" ]; then
-              export ATOM_SCRIPT_NAME="atom"
-              export APM_SCRIPT_NAME="apm"
-            else
-              export ATOM_SCRIPT_NAME="atom-$ATOM_CHANNEL"
-              export APM_SCRIPT_NAME="apm-$ATOM_CHANNEL"
-            fi
-            export ATOM_SCRIPT_PATH="$HOME/atom/usr/bin/$ATOM_SCRIPT_NAME"
-            export APM_SCRIPT_PATH="$HOME/atom/usr/bin/$APM_SCRIPT_NAME"
+            export ATOM_APP_NAME="Atom ${ATOM_CHANNEL}.app"
+            export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
+            export ATOM_SCRIPT_PATH="./atom-${ATOM_CHANNEL}"
+            ln -s "./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh" "${ATOM_SCRIPT_PATH}"
           fi
-
-
-          echo "Using Atom version:"
-          "$ATOM_SCRIPT_PATH" -v
-          echo "Using APM version:"
-          "$APM_SCRIPT_PATH" -v
-
-          echo "Deploying package:"
-          "$APM_SCRIPT_PATH" publish --tag "$TRAVIS_TAG"
+          export PATH="$PWD/atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/bin:$PATH"
+          export ATOM_PATH="./atom"
+          export APM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/apm"
+        else
+          curl -s -L "https://atom.io/download/deb?channel=$ATOM_CHANNEL" \
+            -H 'Accept: application/octet-stream' \
+            -o "atom.deb"
+          /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+          export DISPLAY=":99"
+          dpkg-deb -x atom.deb "$HOME/atom"
+          if [ "$ATOM_CHANNEL" = "stable" ]; then
+            export ATOM_SCRIPT_NAME="atom"
+            export APM_SCRIPT_NAME="apm"
+          else
+            export ATOM_SCRIPT_NAME="atom-$ATOM_CHANNEL"
+            export APM_SCRIPT_NAME="apm-$ATOM_CHANNEL"
+          fi
+          export ATOM_SCRIPT_PATH="$HOME/atom/usr/bin/$ATOM_SCRIPT_NAME"
+          export APM_SCRIPT_PATH="$HOME/atom/usr/bin/$APM_SCRIPT_NAME"
+        fi
         bash
+      end
+
+      def check_atom_version
+        log 'Using Atom version:'
+        context.shell '"$ATOM_SCRIPT_PATH" -v'
+      end
+
+      def check_apm_version
+        log 'Using APM version:'
+        context.shell '"$APM_SCRIPT_PATH" -v'
+      end
+
+      def deploy_package
+        log 'Deploying package:'
+        context.shell '"$APM_SCRIPT_PATH" publish --tag "$TRAVIS_TAG"'
+      end
+
+      def push_app
+        setup_auth
+        install_atom
+        check_atom_version
+        deploy_package
       end
     end
   end
--- spec/provider/apm_spec.rb
+++ spec/provider/apm_spec.rb
@@ -6,10 +6,41 @@ describe DPL::Provider::APM do
     described_class.new(DummyContext.new, :app => 'example', :api_key => 'foo')
   end
 
-  describe '#api' do
-    example 'with an api key' do
-      expect(::Gems).to receive(:key=).with('foo')
-      provider.setup_auth
+  describe '#check_auth' do
+    example do
+      expect(provider).to receive(:setup_auth)
+      provider.check_auth
+    end
+  end
+
+  describe '#install_atom' do
+    example do
+      expect(provider).to receive(:log).with('Downloading latest Atom release...')
+      provider.install_atom
+    end
+  end
+
+  describe '#check_atom_version' do
+    example do
+      expect(provider).to receive(:log).with('Using Atom version:')
+      expect(provider.context).to receive(:shell).with('"$ATOM_SCRIPT_PATH" -v')
+      provider.check_atom_version
+    end
+  end
+
+  describe '#check_apm_version' do
+    example do
+      expect(provider).to receive(:log).with('Using APM version:')
+      expect(provider.context).to receive(:shell).with('"$APM_SCRIPT_PATH" -v')
+      provider.check_apm_version
+    end
+  end
+
+  describe '#deploy_package' do
+    example do
+      expect(provider).to receive(:log).with('Deploying package:')
+      expect(provider.context).to receive(:shell).with('"$APM_SCRIPT_PATH" publish --tag "$TRAVIS_TAG"')
+      provider.deploy_package
     end
   end
 end
