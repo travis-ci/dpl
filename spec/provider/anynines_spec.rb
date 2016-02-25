b9cd4ee8eed3cf378f41be755b1fff82801b2c17
Hiro Asari <asari.ruby@gmail.com>
2016-02-24 15:03:37 -1000

    Update Cloud Foundry tool URL

--- spec/provider/anynines_spec.rb
+++ spec/provider/anynines_spec.rb
@@ -11,7 +11,7 @@ describe DPL::Provider::Anynines do
 
   describe "#check_auth" do
     example do
-      expect(provider.context).to receive(:shell).with('wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-linux-amd64.tgz -qO cf-linux-amd64.tgz && tar -zxvf cf-linux-amd64.tgz && rm cf-linux-amd64.tgz')
+      expect(provider.context).to receive(:shell).with('wget \'https://cli.run.pivotal.io/stable?release=debian64&source=github\' -qO cf-linux-amd64.tgz && tar -zxvf cf-linux-amd64.tgz && rm cf-linux-amd64.tgz')
       expect(provider.context).to receive(:shell).with('./cf api https://api.de.a9s.eu')
       expect(provider.context).to receive(:shell).with('./cf login --u mallomar --p myreallyawesomepassword --o myorg --s outer')
       provider.check_auth
