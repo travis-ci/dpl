ba29343d066ee9579cc377397ceaeaff000dc427
Mathias Rangel Wulff <m@rawu.dk>
2016-04-13 00:15:22 +0200

    Update surge_spec.rb

--- spec/provider/surge_spec.rb
+++ spec/provider/surge_spec.rb
@@ -5,10 +5,10 @@ describe DPL::Provider::Surge do
   subject :provider do
     described_class.new(DummyContext.new, :project => './', :domain => 'mydomain')
   end
-
+ 
   describe "#push_app" do
     it 'should peforme a surge command with correct project and domain set' do
-      expect(provider.context).to receive(:shell).with("surge "+File.expand_path("./")+" mydomain")
+      expect(provider.context).to receive(:shell).with("surge "+File.expand_path(context.env['TRAVIS_BUILD_DIR'] + "/")+" mydomain")
       provider.push_app
     end
   end
