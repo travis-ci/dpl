c504456bfd6ee948aff3f9c52360f87cde19d45e
Matt Knox <matt.knox@sphero.com>
2016-04-25 08:45:28 -0600

    Clean up output from the git push to deis and fix tests.

--- lib/dpl/provider/deis.rb
+++ lib/dpl/provider/deis.rb
@@ -81,7 +81,7 @@ module DPL
       end
 
       def push_app
-        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f"
+        unless context.shell "git push #{verbose_flag} deis HEAD:refs/heads/master -f 2>&1 | tr -dc '[:alnum:][:space:][:punct:]' | sed -E 's/remote: (\\[1G)+//' | sed 's/\\[K$//'"
           error 'Deploying application failed.'
         end
       end
