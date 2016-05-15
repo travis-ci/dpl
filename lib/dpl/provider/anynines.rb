068bb791dd0b4fbc43f561e862e4537fb8b07bf8
Robert Gogolok <gogolok@gmail.com>
2016-05-15 15:45:00 +0200

    fix anynines api

--- lib/dpl/provider/anynines.rb
+++ lib/dpl/provider/anynines.rb
@@ -4,7 +4,7 @@ module DPL
 
       def check_auth
         initial_go_tools_install
-        context.shell "./cf api https://api.de.a9s.eu"
+        context.shell "./cf api https://api.aws.ie.a9s.eu"
         context.shell "./cf login --u #{option(:username)} --p #{option(:password)} --o #{option(:organization)} --s #{option(:space)}"
       end
 
