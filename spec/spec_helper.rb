15c5ff78839806374fcb2ca3f8aa6cbe6bbb295f
Hiro Asari <asari.ruby@gmail.com>
2016-06-09 17:22:34 -0700

    Remove SimpleCov
    It appears to cause build failures. See, for example:
    
    https://travis-ci.org/travis-ci/dpl/jobs/135643499#L660

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,9 @@
-require 'simplecov'
 require 'dpl/error'
 require 'dpl/provider'
 require 'rspec/its'
 require 'coveralls'
 
 Coveralls.wear!
-
-SimpleCov.start do
-  coverage_dir '.coverage'
-
-  add_filter "/spec/"
-  add_group 'Library', 'lib'
-end
 
 class DummyContext
   def shell(command)
