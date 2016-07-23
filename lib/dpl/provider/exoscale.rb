ceb77d599757adef3ff11c35fa5e2e5169b7f61f
Hiro Asari <asari.ruby@gmail.com>
2016-07-22 20:45:21 -0400

    Fix indentation

--- lib/dpl/provider/exoscale.rb
+++ lib/dpl/provider/exoscale.rb
@@ -49,59 +49,60 @@ module DPL
         deploy_app
       end
 
-    private
+      private
 
-    def get_token
-      request = Net::HTTP::Post.new '/api/apps/token'
-      request.basic_auth options[:email], options[:password]
-      response = @tokenHttp.request(request)
-      error('authorization failed') if response.code != '200'
-      return JSON.parse response.body
-    end
+      def get_token
+        request = Net::HTTP::Post.new '/api/apps/token'
+        request.basic_auth options[:email], options[:password]
+        response = @tokenHttp.request(request)
+        error('authorization failed') if response.code != '200'
+        return JSON.parse response.body
+      end
 
-        def get_token
-          request = Net::HTTP::Post.new '/token/'
-          request.basic_auth options[:email], options[:password]
-          response = @http.request(request)
-          error('authorization failed') if response.code != '200'
-          return JSON.parse response.body
+      def get_token
+        request = Net::HTTP::Post.new '/token/'
+        request.basic_auth options[:email], options[:password]
+        response = @http.request(request)
+        error('authorization failed') if response.code != '200'
+        return JSON.parse response.body
+      end
+
+      def headers_with_token(options = {})
+        @token = get_token if options[:new_token] || @token.nil?
+        return {
+          'Authorization' => %Q|cc_auth_token="#{ @token['token'] }"|,
+          'Content-Type' => 'application/json'
+        }
+      end
+
+      def get_headers
+        headers = headers_with_token
+        response = api_call('GET', '/user/', nil, headers)
+        return headers if response.code == '200'
+
+        return headers_with_token :new_token => true
+      end
+
+      def api_call(method, path, data = nil, headers = nil)
+        return @http.send_request(method, path, data, headers || get_headers)
+      end
+
+      def deploy_app
+        data = {'version' => -1}
+        response = api_call('PUT', "/app/#{ app_name }/deployment/#{ dep_name }", JSON.dump(data))
+        error('deployment failed - the deployment is already deploying') if response.code != '200'
+      end
+
+      def user
+        if @user.nil?
+          response = api_call('GET', '/user/')
+          error('can not find the user') if response.code != '200'
+          users = JSON.parse response.body
+          @user = users[0]
         end
+        return @user
+      end
 
-        def headers_with_token(options = {})
-          @token = get_token if options[:new_token] || @token.nil?
-          return {
-            'Authorization' => %Q|cc_auth_token="#{ @token['token'] }"|,
-            'Content-Type' => 'application/json'
-          }
-        end
-
-        def get_headers
-          headers = headers_with_token
-          response = api_call('GET', '/user/', nil, headers)
-          return headers if response.code == '200'
-
-          return headers_with_token :new_token => true
-        end
-
-        def api_call(method, path, data = nil, headers = nil)
-          return @http.send_request(method, path, data, headers || get_headers)
-        end
-
-        def deploy_app
-          data = {'version' => -1}
-          response = api_call('PUT', "/app/#{ app_name }/deployment/#{ dep_name }", JSON.dump(data))
-          error('deployment failed - the deployment is already deploying') if response.code != '200'
-        end
-
-        def user
-          if @user.nil?
-            response = api_call('GET', '/user/')
-            error('can not find the user') if response.code != '200'
-            users = JSON.parse response.body
-            @user = users[0]
-          end
-          return @user
-        end
     end
   end
 end
