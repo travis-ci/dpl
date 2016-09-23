5cb70ba0fc95c04ced755ab4c6b80850db300d2a
Adrian Moreno <adrian@morenomartinez.com>
2016-09-23 15:32:06 +0200

    Fixes Cloud Foundry deployment

--- lib/dpl/provider/cloud_foundry.rb
+++ lib/dpl/provider/cloud_foundry.rb
@@ -9,7 +9,7 @@ module DPL
       def check_auth
         initial_go_tools_install
         context.shell "./cf api #{option(:api)} #{'--skip-ssl-validation' if options[:skip_ssl_validation]}"
-        context.shell "./cf login --u #{option(:username)} --p #{option(:password)} --o #{option(:organization)} --s #{option(:space)}"
+        context.shell "./cf login -u #{option(:username)} -p #{option(:password)} -o #{option(:organization)} -s #{option(:space)}"
       end
 
       def check_app
