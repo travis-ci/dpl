c17aa93e2fc4e386d4974e273d508ebde125a89b
Marwan Rabbâa <waghanza@gmail.com>
2016-12-13 21:30:49 +0100

    delete package before upload on packagecloud

--- lib/dpl/provider/packagecloud.rb
+++ lib/dpl/provider/packagecloud.rb
@@ -22,6 +22,7 @@ module DPL
         @token = option(:token)
         @repo = option(:repository)
         @dist = option(:dist) if options[:dist]
+        @release = option(:release) if options[:release]
         @creds = ::Packagecloud::Credentials.new(@username, @token)
         log "Logging into https://packagecloud.io with #{@username}:#{@token[-4..-1].rjust(20, '*')}"
       end
@@ -108,7 +109,9 @@ module DPL
           end
         end
 
+        force = options.fetch(:package_glob) || false
         packages.each do |package|
+<<<<<<< 4dc9b090e7257691883f5d218531d5d792ee9ff8
           log "Pushing package: #{package.filename}"
           if dist_required?(package.filename)
             result = @client.put_package(@repo, package, get_distro(@dist))
@@ -116,6 +119,15 @@ module DPL
             result = @client.put_package(@repo, package)
           end
 
+=======
+          result = @client.delete_package(@repo, @dist, @release, package.filename) if force
+          if result.succeeded
+            log "Successfully delete #{package.filename} on #{@username}/#{@repo}"
+          else
+            error "Error #{result.response}"
+          end
+          result = @client.put_package(@repo, package, get_distro(@dist))
+>>>>>>> delete package before upload on packagecloud
           if result.succeeded
             log "Successfully pushed #{package.filename} to #{@username}/#{@repo}"
           else
