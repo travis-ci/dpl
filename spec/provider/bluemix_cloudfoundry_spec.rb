cdd370a6e32e23de08f112f14d56227cc070f15b
Adam King <rak@us.ibm.com>
2016-12-14 15:52:53 -0500

    Add Bluemix deploy provider test coverage
    Add spec class to test Bluemix deploy provider
    
    Signed-off-by: Adam King <rak@us.ibm.com>

---
+++ spec/provider/bluemix_cloudfoundry_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'dpl/provider/bluemix_cloudfoundry'
+
+describe DPL::Provider::BluemixCF do
+  subject :provider do
+    described_class.new(DummyContext.new, region: 'eu-gb', username: 'Moonpie',
+                        password: 'myexceptionallyaveragepassword',
+                        organization: 'myotherorg',
+                        space: 'inner',
+                        manifest: 'worker-manifest.yml',
+                        skip_ssl_validation: true)
+  end
+
+  describe "#check_auth" do
+    example do
+      expect(provider.context).to receive(:shell).with('wget \'https://cli.run.pivotal.io/stable?release=linux64-binary&source=github\' -qO cf-linux-amd64.tgz && tar -zxvf cf-linux-amd64.tgz && rm cf-linux-amd64.tgz')
+      expect(provider.context).to receive(:shell).with('./cf api api.eu-gb.bluemix.net --skip-ssl-validation')
+      expect(provider.context).to receive(:shell).with('./cf login -u Moonpie -p myexceptionallyaveragepassword -o myotherorg -s inner')
+      provider.check_auth
+    end
+  end
+
+end
