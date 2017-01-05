56ea2d0bb88b94a9f9519fbd48d53640622d4fdb
Hiro Asari <asari.ruby@gmail.com>
2017-01-05 15:14:01 -0500

    Fix GAE spec to match Python 2.7 invocation

--- spec/provider/gae_spec.rb
+++ spec/provider/gae_spec.rb
@@ -6,9 +6,11 @@ describe DPL::Provider::GAE do
     described_class.new(DummyContext.new, project: 'test')
   end
 
+
   describe '#push_app' do
     example 'with defaults' do
-      allow(provider.context).to receive(:shell).with("#{DPL::Provider::GAE::GCLOUD} --quiet --verbosity \"warning\" --project \"test\" app deploy \"app.yaml\" --promote").and_return(true)
+      expect(provider.context.env).to receive(:[]).with('HOME').and_return('/home/travis')
+      allow(provider.context).to receive(:shell).with("bash -c 'source /home/travis/virtualenv/python2.7/bin/activate; #{DPL::Provider::GAE::GCLOUD} --quiet --verbosity \"warning\" --project \"test\" app deploy \"app.yaml\" --promote'").and_return(true)
       provider.push_app
     end
   end
