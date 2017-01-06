13ccd136fb72ee329174e7c4a184e71688abf1cb
Brandon LeBlanc <demosdemon@gmail.com>
2017-01-05 18:27:17 -0600

    add spec for with_python_2_7 with an apostrophe in `cmd`

--- spec/provider/gae_spec.rb
+++ spec/provider/gae_spec.rb
@@ -14,4 +14,12 @@ describe DPL::Provider::GAE do
       provider.push_app
     end
   end
+
+
+  describe '#with_python_2_7'
+    example 'with apostrophe' do
+        expect(provider.context.env).to receive(:[]).with('HOME').and_return('/home/travis')
+        expect(provider.context).to receive(:shell).with("bash -c 'source /home/travis/virtualenv/python2.7/bin/activate; python -c '\''import sys; print(sys.version)'\''").and_return(true)
+        provider.with_python_2_7("python -c 'import sys; print(sys.version)'")
+    end
 end
