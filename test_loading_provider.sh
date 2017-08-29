ca11fcc4ccf92ea4f93c5268b1bec4a68b9593da
Hiro Asari <asari.ruby@gmail.com>
2017-08-29 11:43:22 -0400

    Test loading all providers

---
+++ test_loading_provider.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+let e=0
+
+for path in lib/dpl/provider/*.rb; do
+  file=$(basename $path)
+  provider=${file%*.rb}
+  echo -e "Testing $provider\n"
+  echo -e "\n\n\n\n" | dpl --provider=$provider --skip-cleanup=true 2>&1 | egrep 'LoadError'
+  if [[ $? -eq 0 ]]; then
+    echo "Failed to load $provider\n"
+    (( e += 1 ))
+  fi
+done
+exit $e
