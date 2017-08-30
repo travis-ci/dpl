82114ef59a41a305ae98b9e4b187434e8a8af1a4
Hiro Asari <asari.ruby@gmail.com>
2017-08-29 21:33:26 -0400

    Create a gemset for each provider

--- test_loading_provider.sh
+++ test_loading_provider.sh
@@ -2,10 +2,17 @@
 
 let e=0
 
+
 for path in lib/dpl/provider/*.rb; do
   file=$(basename $path)
   provider=${file%*.rb}
   echo -e "Testing $provider\n"
+  rvm gemset create $provider
+  rvm gemset use $provider
+
+  set -v
+  gem install dpl-*.gem
+
   echo -e "\n\n\n\n" | dpl --provider=$provider --skip-cleanup=true 2>&1 | egrep 'LoadError'
   if [[ $? -eq 0 ]]; then
     echo "Failed to load $provider\n"
