5348f33698c903431312a22e79f15a34e90f37ac
Dominic Jodoin <dominic@travis-ci.com>
2017-08-31 11:44:44 -0400

    One more time.

--- spec/provider_spec.rb
+++ spec/provider_spec.rb
@@ -99,6 +99,7 @@ describe DPL::Provider do
     example "skip deploy" do
       expect(provider.options).to receive(:[]).with(:skip_deploy).and_return("true")
       expect(provider.options).to receive(:[]).with(:skip_cleanup).and_return("false")
+      allow(provider).to receive_messages(:needs_key? => false)
       expect(provider).not_to receive(:push_app)
       provider.deploy
     end
