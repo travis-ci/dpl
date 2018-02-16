dec584b4abcaf384e3fb292eda36e39fea521d71
Hiro Asari <asari.ruby@gmail.com>
2018-02-16 14:39:32 +0100

    Fix `gem install` invocation

--- lib/dpl/provider.rb
+++ lib/dpl/provider.rb
@@ -61,18 +61,18 @@ module DPL
         raise Error, "could not find provider %p" % opt unless name
 
         begin
+          require "dpl/provider/#{opt_lower}"
           provider = const_get(name).new(context, options)
-        rescue NameError
+        rescue NameError, LoadError
           install_cmd = "gem install dpl-#{opt} -v #{ENV['DPL_VERSION'] || DPL::VERSION}"
 
-          if File.exist?(File.join(File.dirname(__FILE__), '../..', "dpl-#{opt_lower}-*.gem"))
-            install_cmd += " --local"
+          if File.exist?(local_gem = File.join(File.dirname(__FILE__), '../..', "dpl-#{opt_lower}-*.gem"))
+            install_cmd = "gem install #{local_gem}"
           end
 
           context.shell install_cmd
 
           require "dpl/provider/#{opt_lower}"
-
           provider = const_get(name).new(context, options)
         end
 
