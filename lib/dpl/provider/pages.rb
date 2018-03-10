travis-ci/dpl

commit 4f777f0d9c7743aab460c90e7cb3118e6d2e6826

Defer setting gh_token and gh_remote_url until after initialize
To have `dpl --provider=pages --no-deploy` exit cleanly

lib/dpl/provider/pages.rb
@@ -36,7 +36,6 @@ module DPL
 
         @gh_fqdn = fqdn
         @gh_url = options[:github_url] || 'github.com'
-        @gh_token = option(:github_token)
         @keep_history = !!keep_history
         @allow_empty_commit = !!allow_empty_commit
         @committer_from_gh = !!committer_from_gh
@@ -48,11 +47,18 @@ module DPL
         @deployment_file = !!options[:deployment_file]
 
         @gh_ref = "#{@gh_url}/#{slug}.git"
-        @gh_remote_url = "https://#{@gh_token}@#{@gh_ref}"
         @git_push_opts = @keep_history ? '' : ' --force'
         @git_commit_opts = (@allow_empty_commit and @keep_history) ? ' --allow-empty' : ''
 
         print_step "The repo is configured to use committer user and email." if @committer_from_gh
+      end
+
+      def gh_token
+        @gh_token ||= option(:github_token)
+      end
+
+      def gh_remote_url
+        @gh_remote_url ||= "https://#{gh_token}@#{@gh_ref}"
       end
 
       def fqdn

commit 471943af80ffac7095c3fc77476e7473fe11734a

Fix bug introduced by 4f777f0.

lib/dpl/provider/pages.rb
@@ -136,7 +136,7 @@ module DPL
         end
 
         print_step "Trying to clone a single branch #{@target_branch} from existing repo..."
-        unless context.shell "git clone --quiet --branch='#{@target_branch}' --depth=1 '#{@gh_remote_url}' '#{target_dir}' > /dev/null 2>&1"
+        unless context.shell "git clone --quiet --branch='#{@target_branch}' --depth=1 '#{gh_remote_url}' '#{target_dir}' > /dev/null 2>&1"
           # if such branch doesn't exist at remote, init it from scratch
           print_step "Cloning #{@target_branch} branch failed"
           Dir.mkdir(target_dir)  # Restore dir destroyed by failed `git clone`
@@ -180,7 +180,7 @@ module DPL
 
       def github_deploy
         print_step "Doing the git push (workdir: #{Dir.pwd})..."
-        unless context.shell "git push#{@git_push_opts} --quiet '#{@gh_remote_url}' '#{@target_branch}':'#{@target_branch}' > /dev/null 2>&1"
+        unless context.shell "git push#{@git_push_opts} --quiet '#{gh_remote_url}' '#{@target_branch}':'#{@target_branch}' > /dev/null 2>&1"
           error "Couldn't push the build to #{@gh_ref}:#{@target_branch}"
         end
       end