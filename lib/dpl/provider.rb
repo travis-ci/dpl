394d9f5ed7ec9f8ccabb111d563504256cc89eca
Hiro Asari <asari.ruby@gmail.com>
2018-03-19 15:41:01 -0400

    Update git stash docs link [skip ci]

--- lib/dpl/provider.rb
+++ lib/dpl/provider.rb
@@ -220,7 +220,7 @@ module DPL
       context.shell "mv .dpl ~/dpl"
       log "Cleaning up git repository with `git stash --all`. " \
         "If you need build artifacts for deployment, set `deploy.skip_cleanup: true`. " \
-        "See https://docs.travis-ci.com/user/deployment/#Uploading-Files."
+        "See https://docs.travis-ci.com/user/deployment#Uploading-Files-and-skip_cleanup."
       context.shell "git stash --all"
       context.shell "mv ~/dpl .dpl"
     end
