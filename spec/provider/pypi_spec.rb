0c3f63dc6bc9e2c20972213f75b0441b6447c8bc
fgogolli <flamur.gogolli@gmail.com>
2018-04-10 16:59:05 +0100

    Add the option to pin versions for wheel, twine & setuptools

--- spec/provider/pypi_spec.rb
+++ spec/provider/pypi_spec.rb
@@ -15,6 +15,18 @@ describe DPL::Provider::PyPI do
     end
   end
 
+  describe "#install_pinned_deploy_dependencies" do
+    example do
+      provider.options.update(:setuptools_version => '1.0.1')
+      provider.options.update(:twine_version => '1.1.0')
+      provider.options.update(:wheel_version => '0.1.0')
+      expect(provider.context).to receive(:shell).with(
+        "wget -O - https://bootstrap.pypa.io/get-pip.py | python - --no-setuptools --no-wheel && pip install --upgrade setuptools==1.0.1 twine==1.1.0 wheel==0.1.0"
+      ).and_return(true)
+      provider.install_deploy_dependencies
+    end
+  end
+
   describe "#config" do
     it 'accepts a user and a password' do
       expect(provider.config[:servers]['pypi']).to include 'username: foo'
