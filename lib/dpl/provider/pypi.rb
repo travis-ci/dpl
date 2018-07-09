4e314974f49a0c33da3a6cfcc5da62f48e2daa8c
Sorin Sbarnea <ssbarnea@users.noreply.github.com>
2018-07-09 17:12:56 +0100

    pypi: install packages using --user
    Avoids failure to install due to lack of sudo access.

--- lib/dpl/provider/pypi.rb
+++ lib/dpl/provider/pypi.rb
@@ -67,7 +67,7 @@ module DPL
 
       def install_deploy_dependencies
         unless context.shell "wget -O - https://bootstrap.pypa.io/get-pip.py | python - --no-setuptools --no-wheel && " \
-                             "pip install --upgrade #{pypi_setuptools_arg} #{pypi_twine_arg} #{pypi_wheel_arg}"
+                             "pip install --user --upgrade #{pypi_setuptools_arg} #{pypi_twine_arg} #{pypi_wheel_arg}"
           error "Couldn't install pip, setuptools, twine or wheel."
         end
       end
