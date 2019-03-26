585e68bfae0d54d539767db92458b7bbf6e6cfb9
Hiro Asari <asari.ruby@gmail.com>
2019-03-26 17:54:11 -0400

    Remove indirect dependencies from chef-supermarket_spec.rb
    So that `require` issues can be detected sooner.

--- spec/provider/chef_supermarket_spec.rb
+++ spec/provider/chef_supermarket_spec.rb
@@ -1,7 +1,4 @@
 require 'spec_helper'
-require 'chef/cookbook_loader'
-require 'chef/cookbook_uploader'
-require 'chef/cookbook_site_streaming_uploader'
 require 'dpl/provider/chef_supermarket'
 
 describe DPL::Provider::ChefSupermarket do
