e1a8ac569d625c4908974bc7004d68f46dfbf55a
Sven Fuchs <me@svenfuchs.com>
2019-05-17 01:45:23 +0200

    use enum values if present

--- .travis/test_install.rb
+++ .travis/test_install.rb
@@ -3,9 +3,14 @@ require 'dpl'
 keys = Dpl::Provider.registry.keys.sort - %i(chef_supermarket help heroku provider)
 providers = keys.map { |key| Dpl::Provider[key] }
 
+def opt_for(opt)
+  "--#{opt.name} #{opt.enum? ? opt.enum.first : 'str'}"
+end
+
 providers.each do |provider|
   opts = provider.opts.select(&:required?)
-  opts = opts.map { |opt| "--#{opt.name} str" }.join(' ')
+  opts = opts.map { |opt| opt_for(opt) }.join(' ')
   cmd = "bin/dpl #{provider.registry_key} --stage install #{opts}"
+  puts cmd
   fail unless system cmd
 end
