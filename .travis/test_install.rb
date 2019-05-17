bf363f7f9c9fa69b9afd0decc8234e01e1e76237
Sven Fuchs <me@svenfuchs.com>
2019-05-18 00:44:15 +0200

    try requiring bundler < 2.0 for ruby 2.2

--- .travis/test_install.rb
+++ .travis/test_install.rb
@@ -1,61 +1,31 @@
 require 'dpl'
 
-# passing = %i(
-#   anynines
-#   atlas
-#   azure_web_apps
-#   bit_balloon
-#   bluemix_cloud_foundry
-#   boxfuse
-#   cargo
-#   catalyze
-#   cloud66
-#   cloud_files
-#   cloud_foundry
-#   code_deploy
-#   deis
-#   elastic_beanstalk
-#   engine_yard
-#   firebase
-#   gae
-#   gcs
-#   hackage
-#   hephy
-#   lambda
-#   launchpad
-#   npm
-#   open_shift
-#   ops_works
-#   packagecloud
-#   pages
-#   puppet_forge
-#   pypi
-#   releases
-#   rubygems
-#   s3
-#   scalingo
-#   script
-#   snap
-#   surge
-#   testfairy
-#   transifex
-# )
+def clear_gems
+  run 'gem uninstall -aIx &2>1'
+  run 'gem install cl > /dev/null &2>1'
+end
 
-skip = %i(help heroku provider)
-keys = Dpl::Provider.registry.keys.sort - skip # - passing
-providers = keys.map { |key| Dpl::Provider[key] }
+def opts_for(provider)
+  opts = provider.opts.select(&:required?)
+  opts = opts + [provider.opts[provider.required.map(&:first).first]].compact
+  opts.map { |opt| opt_for(opt) }.join(' ')
+end
 
 def opt_for(opt)
   "--#{opt.name} #{opt.enum? ? opt.enum.first : 'str'}"
 end
 
-providers.each do |provider|
-  system 'gem uninstall -aIx'
-  system 'gem install cl'
-  opts = provider.opts.select(&:required?)
-  opts = opts + [provider.opts[provider.required.map(&:first).first]].compact
-  opts = opts.map { |opt| opt_for(opt) }.join(' ')
-  cmd = "bin/dpl #{provider.registry_key} --stage install #{opts}"
+def run(cmd)
+  puts
   puts cmd
   fail unless system cmd
+end
+
+skip = %i(help heroku provider)
+keys = Dpl::Provider.registry.keys.sort - skip
+providers = keys.map { |key| Dpl::Provider[key] }
+
+providers.each do |provider|
+  clear_gems
+  run "bin/dpl #{provider.registry_key} --stage install #{opts_for(provider)}"
 end
