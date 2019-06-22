2bcf90e9a822d8cc6760d534f03b2319f25ead93
Sven Fuchs <me@svenfuchs.com>
2019-06-22 21:17:28 +0200

    try boxfuse [ci:skip]

--- spec/dpl/providers/boxfuse_spec.rb
+++ spec/dpl/providers/boxfuse_spec.rb
@@ -1,6 +1,11 @@
 describe Dpl::Providers::Boxfuse do
   before { subject.run }
 
+  # after do
+  #   ENV.delete('BOXFUSE_USER')
+  #   ENV.delete('BOXFUSE_SECRET')
+  # end
+
   describe 'install' do
     it { should have_run %r(curl -L https://files.boxfuse.com/.*.tar.gz | tar xz) }
   end
@@ -10,32 +15,32 @@ describe Dpl::Providers::Boxfuse do
   end
 
   describe 'given --user user' do
-    it { should have_run 'boxfuse/boxfuse run --user="user"' }
+    it { should have_run 'boxfuse/boxfuse run -user="user"' }
   end
 
   describe 'given --secret secret' do
-    it { should have_run 'boxfuse/boxfuse run --secret="secret"' }
+    it { should have_run 'boxfuse/boxfuse run -secret="secret"' }
   end
 
   describe 'given --config_file ./file' do
-    it { should have_run 'boxfuse/boxfuse run --configfile=./file' }
+    it { should have_run 'boxfuse/boxfuse run -configfile=./file' }
   end
 
   describe 'given --configfile ./file' do
-    it { should have_run 'boxfuse/boxfuse run --configfile=./file' }
+    it { should have_run 'boxfuse/boxfuse run -configfile=./file' }
     it { should have_deprecated :configfile }
   end
 
   describe 'given --payload payload' do
-    it { should have_run 'boxfuse/boxfuse run --payload="payload"' }
+    it { should have_run 'boxfuse/boxfuse run -payload="payload"' }
   end
 
   describe 'given --image image' do
-    it { should have_run 'boxfuse/boxfuse run --image="image"' }
+    it { should have_run 'boxfuse/boxfuse run -image="image"' }
   end
 
   describe 'given --env env' do
-    it { should have_run 'boxfuse/boxfuse run --env="env"' }
+    it { should have_run 'boxfuse/boxfuse run -env="env"' }
   end
 
   describe 'given --extra_args args' do
