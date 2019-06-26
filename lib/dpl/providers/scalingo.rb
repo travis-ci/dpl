f62b2ecad8d4a8f053d9f9af80663ce45fec514e
Sven Fuchs <me@svenfuchs.com>
2019-06-26 15:33:40 +0200

    try scalingo [ci:skip]

--- lib/dpl/providers/scalingo.rb
+++ lib/dpl/providers/scalingo.rb
@@ -7,25 +7,26 @@ module Dpl
 
       required :api_key, [:username, :password]
 
+      opt '--app APP', default: :repo_name
       opt '--api_key KEY', 'scalingo API key', alias: :api_token, deprecated: :api_token
       opt '--username NAME', 'scalingo username'
       opt '--password PASS', 'scalingo password'
-      opt '--remote REMOTE', 'Remote url or git remote name of your git repository.', default: 'scalingo'
-      opt '--branch BRANCH', 'Branch of your git repository.', default: 'master'
-      opt '--app APP', 'Required if your repository does not contain the appropriate remote (will add a remote to your local repository)'
+      opt '--remote REMOTE', 'Git remote name', default: 'scalingo'
+      opt '--branch BRANCH', 'Git branch', default: 'master'
 
       needs :git, :ssh_key
 
-      cmds login_key:   'timeout 2 ./scalingo login --api-token %{api_key} 2> /dev/null > /dev/null',
-           login_creds: 'echo -e \"%{username}\n%{password}\" | timeout 2 ./scalingo login 2> /dev/null > /dev/null',
+      cmds login_key:   'timeout 2 ./scalingo login --api-token %{api_key} > /dev/null 2>&1',
+           login_creds: 'echo -e \"%{username}\n%{password}\" | timeout 2 ./scalingo login > /dev/null 2>&1',
            add_key:     './scalingo keys-add dpl_tmp_key %s',
            remove_key:  './scalingo keys-remove dpl_tmp_key',
-           remote_add:  'git remote add %{remote} git@scalingo.com:%{app}.git 2> /dev/null > /dev/null',
-           push:        'git push %{remote} %{branch} -f'
+           remote_add:  'git remote add %{remote} git@scalingo.com:%{app}.git > /dev/null 2>&1',
+           push:        'git push %{remote} HEAD:%{branch} -f'
 
       errs install:    'Failed to install the Scalingo CLI.',
-           login:      'Failed to authenticate with the Scalingo API',
+           login:      'Failed to authenticate with the Scalingo API.',
            add_key:    'Failed to add the ssh key.',
+           remote_add: 'Failed to add the git remote.',
            push:       'Failed to push the app.',
            remove_key: 'Failed to remove the ssh key.'
 
@@ -38,19 +39,19 @@ module Dpl
       end
 
       def add_key(file, type = nil)
-        shell :add_key, file, assert: true
+        shell :add_key, file, assert: true, echo: true
       end
 
       def setup
-        shell :remote_add if app
+        shell :remote_add, assert: true, echo: true
       end
 
       def deploy
-        shell :push, assert: true
+        shell :push, assert: true, echo: true
       end
 
       def remove_key
-        shell :remove_key, assert: true
+        shell :remove_key, assert: true, echo: true
       end
     end
   end
