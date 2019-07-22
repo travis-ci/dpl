a1aa13337286f12ddc58d2faae08dee77cc8ef03
Sven Fuchs <me@svenfuchs.com>
2019-07-22 23:50:52 +0200

    catalyze -> datica

--- spec/dpl/providers/datica_spec.rb
+++ spec/dpl/providers/datica_spec.rb
@@ -1,4 +1,4 @@
-describe Dpl::Providers::Catalyze do
+describe Dpl::Providers::Datica do
   let(:args) { |e| %w(--target target) + args_from_description(e) }
 
   describe 'by default' do
@@ -17,5 +17,11 @@ describe Dpl::Providers::Catalyze do
     it { should have_run 'git checkout HEAD' }
     it { should have_run 'git add . --all --force' }
     it { should have_run 'git commit -m "Build #1 (commit) of repo@branch" --quiet' }
+  end
+
+  describe 'using alias registry key :catalyze' do
+    let(:provider) { :catalyze }
+    before { subject.run }
+    it { should have_run 'git push --force target HEAD:master' }
   end
 end
