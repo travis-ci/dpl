01a3af2f39b357ca3227c463ad8665d79f32278d
Sven Fuchs <me@svenfuchs.com>
2019-08-01 18:48:08 +0200

    move deis to ~/.dpl and add ~/.dpl to the PATH

--- spec/dpl/providers/hephy_spec.rb
+++ spec/dpl/providers/hephy_spec.rb
@@ -4,15 +4,13 @@ describe Dpl::Providers::Hephy do
   before { subject.run }
 
   describe 'by default', record: true do
-    it { should have_run 'curl -sSL https://raw.githubusercontent.com/teamhephy/workflow-cli/master/install-v2.sh | bash -x -s stable' }
-    it { should have_run './deis login hephy.hephyapps.com --username=user --password=pass' }
-    it { should have_run './deis keys:add ~/.dpl/id_rsa.pub' }
+    it { should have_run 'curl -sSL https://raw.githubusercontent.com/teamhephy/workflow-cli/master/install-v2.sh | bash -x -s stable && mv deis ~/.dpl' }
+    it { should have_run 'deis login hephy.hephyapps.com --username=user --password=pass' }
+    it { should have_run 'deis keys:add ~/.dpl/id_rsa.pub' }
     it { should have_run %r(dpl/.dpl/git-ssh hephy-builder.hephyapps.com -p 2222  2>&1 | grep -c 'PTY allocation request failed' > /dev/null) }
-    it { should have_run 'mv ./deis ~/deis' }
     it { should have_run 'git stash --all' }
-    it { should have_run 'mv ~/deis ./deis' }
     it { should have_run %r(git push.*ssh://git@hephy-builder.hephyapps.com:2222/app.git) }
-    it { should have_run "./deis keys:remove machine_name" }
+    it { should have_run "deis keys:remove machine_name" }
     it { should have_run 'git stash pop' }
     it { should have_run_in_order }
   end
