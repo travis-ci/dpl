3f689ac8051d8dc9c47f23060dc74ebf182bc462
Sven Fuchs <me@svenfuchs.com>
2019-08-11 01:52:48 +0200

    add firebase --force (ports #949)

--- lib/dpl/providers/firebase.rb
+++ lib/dpl/providers/firebase.rb
@@ -19,6 +19,7 @@ module Dpl
       opt '--project NAME',  'Firebase project to deploy to (defaults to the one specified in your firebase.json)'
       opt '--message MSG',   'Message describing this deployment.'
       opt '--only SERVICES', 'Firebase services to deploy', note: 'can be a comma-separated list'
+      opt '--force',         'Whether or not to delete Cloud Functions missing from the current working directory'
 
       cmds deploy: 'firebase deploy --non-interactive %{deploy_opts}'
       errs deploy: 'Firebase deployment failed'
@@ -33,7 +34,7 @@ module Dpl
       end
 
       def deploy_opts
-        opts_for(%i(project message token only))
+        opts_for(%i(project message token only force))
       end
     end
   end
