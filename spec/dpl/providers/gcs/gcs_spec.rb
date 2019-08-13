ac561a9bfd0d4007380ea3deedab6f5e34223628
Hiro Asari <asari.ruby@gmail.com>
2019-08-13 16:31:26 -0400

    Remove irrelevant examples

--- spec/dpl/providers/gcs/gcs_spec.rb
+++ spec/dpl/providers/gcs/gcs_spec.rb
@@ -5,15 +5,7 @@ describe Dpl::Providers::Gcs::Gcs do
   before { subject.run }
 
   describe 'by default', record: true do
-    it { should have_run 'mv /etc/boto.cfg /tmp/boto.cfg' }
-    it { should have_run '[validate:runtime] python (>= 2.7.9)' }
-    it { should have_run 'curl -L https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-252.0.0-linux-x86_64.tar.gz | tar xz -C ~ && ~/google-cloud-sdk/install.sh --path-update false --usage-reporting false --command-completion false' }
-    it { should have_run '[info] Authenticating with access key: i*******************' }
-    it { should have_run 'gsutil cp -r one gs://bucket/' }
-    it { should have_run 'gsutil cp -r two/two gs://bucket/' }
-    it { should have_run 'mv /tmp/boto.cfg /etc/boto.cfg' }
     it { should have_run_in_order }
-    it { should_not have_run 'gsutil cp -r .hidden gs://bucket/' }
   end
 
 end
