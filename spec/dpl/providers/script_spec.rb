293d35353d17892a4e5b6a5ce337cd7092ba75d7
Sven Fuchs <me@svenfuchs.com>
2019-08-18 15:07:02 +0200

    allow multiple cmds on script (see #673)

--- spec/dpl/providers/script_spec.rb
+++ spec/dpl/providers/script_spec.rb
@@ -3,8 +3,8 @@ describe Dpl::Providers::Script do
 
   before { subject.run }
 
-  describe 'given --script ./script' do
-    it { should have_run './script' }
+  describe 'given --script ./one --script ./two' do
+    it { should have_run './one' }
+    it { should have_run './two' }
   end
 end
-
