423b73c6860d60acee85a3d56bf6532d19f8f2ee
Sven Fuchs <me@svenfuchs.com>
2019-08-18 18:19:48 +0200

    unnest s3 specs

--- spec/dpl/providers/s3_spec.rb
+++ spec/dpl/providers/s3_spec.rb
@@ -38,83 +38,80 @@ describe Dpl::Providers::S3 do
     end
   end
 
+  before { |c| subject.run unless c.metadata[:run].is_a?(FalseClass) }
 
-  context do
-    before { subject.run }
-
-    describe 'by default', record: true do
-      it { should have_run '[info] Using Access Key: ac******************' }
-      it { should have_run '[info] Uploading 1 files with up to 5 threads.' }
-      it { should have_run '[info] Uploading file one.txt to / with acl=private content_type=text/plain cache_control=no-cache storage_class=STANDARD' }
-      it { should have_run_in_order }
-      it { should put_object 'one.txt', host: 'bucket.s3.amazonaws.com', 'x-amz-acl': 'private', 'cache-control': 'no-cache', 'x-amz-storage-class': 'STANDARD' }
-    end
-
-    describe 'given --endpoint https://host.com' do
-      it { should put_object 'one.txt', host: 'bucket.host.com' }
-    end
-
-    describe 'given --region us-west-1' do
-      it { should put_object 'one.txt', host: 'bucket.s3.us-west-1.amazonaws.com' }
-    end
-
-    describe 'given --upload_dir dir' do
-      it { should have_run %r(Uploading file one.txt to dir) }
-      it { should put_object 'one.txt', path: '/dir/one.txt' }
-    end
-
-    describe 'given --dot_match' do
-      it { should have_run %r(.hidden.txt) }
-      it { should put_object '.hidden.txt' }
-    end
-
-    describe 'given --storage_class STANDARD_IA' do
-      it { should have_run %r(one.txt.* storage_class=STANDARD_IA) }
-      it { should put_object 'one.txt', 'x-amz-storage-class': 'STANDARD_IA' }
-    end
-
-    describe 'given --acl public_read' do
-      it { should have_run %r(one.txt.* acl=public-read) }
-      it { should put_object 'one.txt', 'x-amz-acl': 'public-read' }
-    end
-
-    describe 'given --cache_control public' do
-      it { should have_run %r(one.txt.* cache_control=public) }
-      it { should put_object 'one.txt', 'cache-control': 'public' }
-    end
-
-    describe 'given --cache_control max-age=60' do
-      it { should have_run %r(one.txt.* cache_control=max-age=60) }
-      it { should put_object 'one.txt', 'cache-control': 'max-age=60' }
-    end
-
-    describe 'given --expires "2020-01-01 00:00:00 UTC"' do
-      it { should have_run %r(one.txt.* expires=2020-01-01 00:00:00 UTC) }
-      it { should put_object 'one.txt', 'expires': 'Wed, 01 Jan 2020 00:00:00 GMT' }
-    end
-
-    describe 'given --default_text_charset utf-8' do
-      it { should have_run %r(one.txt.* content_type=text/plain; charset=utf-8) }
-      it { should put_object 'one.txt', 'content-type': 'text/plain; charset=utf-8' }
-    end
-
-    describe 'given --server_side_encryption' do
-      it { should have_run %r(one.txt.* server_side_encryption=AES256) }
-      it { should put_object 'one.txt', 'x-amz-server-side-encryption': 'AES256' }
-    end
-
-    describe 'given --detect_encoding' do
-      it { should have_run %r(one.txt.* content_encoding=text) }
-      it { should put_object 'one.txt', 'content-encoding': 'text' }
-    end
-
-    describe 'given --index_document_suffix suffix' do
-      it { should have_run '[info] Setting index document suffix to suffix' }
-      it { should put_bucket_website_suffix 'suffix' }
-    end
+  describe 'by default', record: true do
+    it { should have_run '[info] Using Access Key: ac******************' }
+    it { should have_run '[info] Uploading 1 files with up to 5 threads.' }
+    it { should have_run '[info] Uploading file one.txt to / with acl=private content_type=text/plain cache_control=no-cache storage_class=STANDARD' }
+    it { should have_run_in_order }
+    it { should put_object 'one.txt', host: 'bucket.s3.amazonaws.com', 'x-amz-acl': 'private', 'cache-control': 'no-cache', 'x-amz-storage-class': 'STANDARD' }
   end
 
-  describe 'given --local_dir two' do
+  describe 'given --endpoint https://host.com' do
+    it { should put_object 'one.txt', host: 'bucket.host.com' }
+  end
+
+  describe 'given --region us-west-1' do
+    it { should put_object 'one.txt', host: 'bucket.s3.us-west-1.amazonaws.com' }
+  end
+
+  describe 'given --upload_dir dir' do
+    it { should have_run %r(Uploading file one.txt to dir) }
+    it { should put_object 'one.txt', path: '/dir/one.txt' }
+  end
+
+  describe 'given --dot_match' do
+    it { should have_run %r(.hidden.txt) }
+    it { should put_object '.hidden.txt' }
+  end
+
+  describe 'given --storage_class STANDARD_IA' do
+    it { should have_run %r(one.txt.* storage_class=STANDARD_IA) }
+    it { should put_object 'one.txt', 'x-amz-storage-class': 'STANDARD_IA' }
+  end
+
+  describe 'given --acl public_read' do
+    it { should have_run %r(one.txt.* acl=public-read) }
+    it { should put_object 'one.txt', 'x-amz-acl': 'public-read' }
+  end
+
+  describe 'given --cache_control public' do
+    it { should have_run %r(one.txt.* cache_control=public) }
+    it { should put_object 'one.txt', 'cache-control': 'public' }
+  end
+
+  describe 'given --cache_control max-age=60' do
+    it { should have_run %r(one.txt.* cache_control=max-age=60) }
+    it { should put_object 'one.txt', 'cache-control': 'max-age=60' }
+  end
+
+  describe 'given --expires "2020-01-01 00:00:00 UTC"' do
+    it { should have_run %r(one.txt.* expires=2020-01-01 00:00:00 UTC) }
+    it { should put_object 'one.txt', 'expires': 'Wed, 01 Jan 2020 00:00:00 GMT' }
+  end
+
+  describe 'given --default_text_charset utf-8' do
+    it { should have_run %r(one.txt.* content_type=text/plain; charset=utf-8) }
+    it { should put_object 'one.txt', 'content-type': 'text/plain; charset=utf-8' }
+  end
+
+  describe 'given --server_side_encryption' do
+    it { should have_run %r(one.txt.* server_side_encryption=AES256) }
+    it { should put_object 'one.txt', 'x-amz-server-side-encryption': 'AES256' }
+  end
+
+  describe 'given --detect_encoding' do
+    it { should have_run %r(one.txt.* content_encoding=text) }
+    it { should put_object 'one.txt', 'content-encoding': 'text' }
+  end
+
+  describe 'given --index_document_suffix suffix' do
+    it { should have_run '[info] Setting index document suffix to suffix' }
+    it { should put_bucket_website_suffix 'suffix' }
+  end
+
+  describe 'given --local_dir two', run: false do
     dir 'two'
     file 'two/two.txt'
 
@@ -125,7 +122,7 @@ describe Dpl::Providers::S3 do
     it { should put_object 'two.txt' }
   end
 
-  describe 'with ~/.aws/credentials' do
+  describe 'with ~/.aws/credentials', run: false do
     let(:args) { |e| %w(--bucket bucket) }
 
     file '~/.aws/credentials', <<-str.sub(/^\s*/, '')
@@ -138,7 +135,7 @@ describe Dpl::Providers::S3 do
     it { should have_run '[info] Using Access Key: ac******************' }
   end
 
-  describe 'with ~/.aws/config' do
+  describe 'with ~/.aws/config', run: false do
     let(:args) { |e| %w(--access_key_id id --secret_access_key key) }
 
     file '~/.aws/config', <<-str.sub(/^\s*/, '')
