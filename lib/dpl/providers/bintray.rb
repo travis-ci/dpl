501dac967996aa8c9d04f83104c96c8b072bbc54
Albertin Loic <loic.albertin@atos.net>
2019-08-19 17:31:00 +0200

    Specify content type when publishing file in dl list

--- lib/dpl/providers/bintray.rb
+++ lib/dpl/providers/bintray.rb
@@ -124,7 +124,7 @@ module Dpl
 
       def update_file(file)
         retrying(max: 10, pause: 5) do
-	put(path(:file_metadata, target: file.target), {list_in_downloads: file.download}.to_json)
+          put(path(:file_metadata, target: file.target), {list_in_downloads: file.download}.to_json, {}, 'application/json')
         end
       end
 
@@ -192,8 +192,9 @@ module Dpl
         request(req)
       end
 
-      def put(path, body, params = {})
+      def put(path, body, params = {}, contentType = '')
         req = Net::HTTP::Put.new(append_params(path, params))
+        req.add_field('Content-Type', contentType) if contentType != ''
         req.basic_auth(user, key)
         req.body = body
         request(req)
