7be003d29fb804ca599a3a993ec700eb13118091
Sven Fuchs <me@svenfuchs.com>
2019-09-10 17:17:55 +0200

    add cargo --allow_dirty

--- lib/dpl/providers/cargo.rb
+++ lib/dpl/providers/cargo.rb
@@ -10,12 +10,19 @@ module Dpl
       env :cargo
 
       opt '--token TOKEN', 'Cargo registry API token', required: true, secret: true
+      opt '--allow_dirty', 'Allow cargo to publish from a dirty git working directory'
 
-      cmds publish: 'cargo publish --token %{token}'
+      cmds publish: 'cargo publish %{publish_opts}'
 
       def deploy
         shell :publish
       end
+
+      private
+
+        def publish_opts
+          opts_for(%i(token allow_dirty), dashed: true)
+        end
     end
   end
 end
--- spec/dpl/providers/cargo_spec.rb
+++ spec/dpl/providers/cargo_spec.rb
@@ -2,8 +2,13 @@ describe Dpl::Providers::Cargo do
   before { |c| subject.run if run?(c) }
 
   describe 'given --token 1234' do
-    it { should have_run '[info] $ cargo publish --token 1*******************' }
-    it { should have_run 'cargo publish --token 1234' }
+    it { should have_run '[info] $ cargo publish --token="1*******************"' }
+    it { should have_run 'cargo publish --token="1234"' }
+  end
+
+  describe 'given --token 1234 --allow_dirty' do
+    it { should have_run '[info] $ cargo publish --token="1*******************" --allow-dirty' }
+    it { should have_run 'cargo publish --token="1234" --allow-dirty' }
   end
 
   describe 'with credentials in env vars', run: false do
