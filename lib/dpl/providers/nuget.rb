19cb48235787cc2c2b062c9ab3f18070a7375e11
Robert <robert.warmack@gmail.com>
2020-01-17 12:19:47 -0800

    Handle options correctly.

--- lib/dpl/providers/nuget.rb
+++ lib/dpl/providers/nuget.rb
@@ -14,28 +14,31 @@ module Dpl
       opt '--api_key KEY', 'NuGet registry api key', required: true, secret: true, note: 'can be retrieved from your NuGet registry provider', see: 'https://docs.npmjs.com/creating-and-viewing-authentication-tokens'
       opt '--registry URL', 'NuGet registry url', required: true, note: 'ex: "https://www.myget.org/F/org-name/api/v2/package"'
       opt '--src SRC', 'nupkg file(s) to push', default: '*.nupkg' 
-      opt '--no-symbols', note: 'does not push symbols (even if present)'
+      opt '--no_symbols', note: 'does not push symbols (even if present)'
       #opt '--no-service-endpoint', 'does not append "api/v2/package" to the source URL'
-      opt '--skip-duplicate', 'does not push packages with 409 Conflict response from the server'
+      opt '--skip_duplicate', 'does not push packages with 409 Conflict response from the server'
 
       msgs login:    'Authenticating with API key %{api_key}',
            push:     'Pushing package %{src} to %{registry}'
            #version:  '.NET version: %{dotnet_version}',
 
-      cmds push:  'dotnet nuget push %{src} -k %{api_key} -s %{registry} %{push_opts}'
+      cmds push:  'dotnet nuget push %{src} -k %{api_key} -s %{registry} %{no_symbols_option} %{skip_duplicate_option}'
 
       errs push:  'Failed to push'
 
       def deploy
         info :login
-        info :push
         shell :push
       end
 
       private
 
-        def push_opts
-          opts_for(%i(no-symbols skip-duplicates), dashed: true)
+        def no_symbols_option
+          '--no-symbols' if no_symbols?
+        end
+
+        def skip_duplicate_option
+          '--skip-duplicate' if skip_duplicate?
         end
 
     end
