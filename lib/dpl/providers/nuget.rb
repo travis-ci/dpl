a6a2981439b413b86f4d92d3ecdc468707fa2e43
Sven Fuchs <me@svenfuchs.com>
2020-02-26 18:01:36 +0100

    add specs

--- lib/dpl/providers/nuget.rb
+++ lib/dpl/providers/nuget.rb
@@ -12,19 +12,17 @@ module Dpl
       env :nuget, :dotnet
 
       opt '--api_key KEY', 'NuGet registry api key', required: true, secret: true, note: 'can be retrieved from your NuGet registry provider', see: 'https://docs.npmjs.com/creating-and-viewing-authentication-tokens'
-      opt '--registry URL', 'NuGet registry url', required: true, note: 'ex: "https://www.myget.org/F/org-name/api/v2/package"'
-      opt '--src SRC', 'nupkg file(s) to push', default: '*.nupkg' 
+      opt '--registry URL', 'NuGet registry url', required: true, eg: 'https://www.myget.org/F/org-name/api/v2/package'
+      opt '--src SRC', 'nupkg file(s) to push', default: '*.nupkg'
       opt '--no_symbols', note: 'does not push symbols (even if present)'
-      #opt '--no-service-endpoint', 'does not append "api/v2/package" to the source URL'
       opt '--skip_duplicate', 'does not push packages with 409 Conflict response from the server'
 
-      msgs login:    'Authenticating with API key %{api_key}',
-           push:     'Pushing package %{src} to %{registry}'
-           #version:  '.NET version: %{dotnet_version}',
+      msgs login: 'Authenticating with API key %{api_key}',
+           push:  'Pushing package %{src} to %{registry}'
 
-      cmds push:  'dotnet nuget push %{src} -k %{api_key} -s %{registry} %{no_symbols_option} %{skip_duplicate_option}'
+      cmds push: 'dotnet nuget push %{src} -k %{api_key} -s %{registry} %{no_symbols_option} %{skip_duplicate_option}'
 
-      errs push:  'Failed to push'
+      errs push: 'Failed to push'
 
       def deploy
         info :login
