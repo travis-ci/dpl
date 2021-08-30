c169b449c8284c342d1ffad8c915fd49801f13c7
Lukas Elmer <lukas.elmer@gmail.com>
2021-08-30 19:24:59 +0200

    Update firebase.rb

--- lib/dpl/providers/firebase.rb
+++ lib/dpl/providers/firebase.rb
@@ -9,9 +9,9 @@ module Dpl
         tbd
       str
 
-      node_js '>= 8.0.0'
+      node_js '>= 10.13.0'
 
-      npm 'firebase-tools@^6.3', 'firebase'
+      npm 'firebase-tools@^9.16', 'firebase'
 
       path 'node_modules/.bin'
 
@@ -21,9 +21,11 @@ module Dpl
       opt '--project NAME',  'Firebase project to deploy to (defaults to the one specified in your firebase.json)'
       opt '--message MSG',   'Message describing this deployment.'
       opt '--only SERVICES', 'Firebase services to deploy', note: 'can be a comma-separated list'
+      opt '--except SERVICES', 'Firebase services to not deploy', note: 'can be a comma-separated list'
+      opt '--public PATH',   'Override the Hosting public directory specified in firebase.json'
       opt '--force',         'Whether or not to delete Cloud Functions missing from the current working directory'
 
-      cmds deploy: 'firebase deploy --non-interactive %{deploy_opts}'
+      cmds deploy: 'firebase deploy %{deploy_opts}'
       errs deploy: 'Firebase deployment failed'
       msgs missing_config: 'Missing firebase.json'
 
@@ -36,7 +38,7 @@ module Dpl
       end
 
       def deploy_opts
-        opts_for(%i(project message token only force))
+        opts_for(%i(project message token only except public force))
       end
     end
   end
